b2c366904c40d5dc244df6b858bb98eefea8fb1e
NicolasCastela <nickcastela2022@hotmail.com>
2024-02-02 10:51:12 -0400

    att env, create table, crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { Prisma\]Module } from './prisma/]/prisma/].module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, Prisma, PrismaModule\]Module],
   controllers: [AppController],
   providers: [AppService],
 })
